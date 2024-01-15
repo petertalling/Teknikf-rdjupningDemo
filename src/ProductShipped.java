378812bd8f91e8ffd27bce0bfe7668360f19b156
Peter Talling <114860201+petertalling@users.noreply.github.com>
2024-01-15 11:25:17 +0100

    snapshots save and clean up

--- src/ProductShipped.java
+++ src/ProductShipped.java
@@ -1,20 +1,10 @@
 import java.time.LocalDateTime;
 
 public record ProductShipped(String item, int quantity, LocalDateTime dateTime) implements EventInterface {
-    public ProductShipped(String item, int quantity, LocalDateTime dateTime) {
-        this.item = item;
-        this.quantity = quantity;
-        this.dateTime = dateTime;
-    }
-
-    public String item() {
-        return this.item;
-    }
 
     public int quantity() {
         return this.quantity;
     }
-
     public LocalDateTime dateTime() {
         return this.dateTime;
     }
